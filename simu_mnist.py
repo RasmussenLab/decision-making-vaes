a33ee354ff7998a3c251ad24e9590c2e5d47fdd9
enryh <henry.webel@cpr.ku.dk>
2022-05-09 09:49:07 +0200

    :bug: deactivate hard coded device options
    - two specific environment variables were set

--- simu_mnist.py
+++ simu_mnist.py
@@ -6,8 +6,8 @@ import os
 import logging
 from math import ceil
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 import numpy as np
 import pandas as pd
